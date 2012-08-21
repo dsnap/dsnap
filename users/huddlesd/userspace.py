cfdf64d28a7f96abd246e1fde78b1aea0700ce8a
huddlesd <root@CapstoneDev.(none)>
2012-08-20 22:49:08 -0700

    Modified 'defaultTranslator' method to reverse hex output.

--- users/huddlesd/userspace.py
+++ users/huddlesd/userspace.py
@@ -205,7 +205,8 @@ def addTranslator(typeString, translator):
 ##
 def defaultTranslator(rawValue):
 
-	return ''.join(["%02X " % ord(x) for x in rawValue]).strip()
+#	return ''.join(["%02X " % ord(x) for x in rawValue]).strip()
+	return ''.join(["%02X " % ord(x) for x in reversed(rawValue)].strip()
 
 
 ## Type-specific defintions.
