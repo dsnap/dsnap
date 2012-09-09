3acf60649db5146c40090da573a6053b9d282460
Jacob Sowles <jacob.r.sowles@intel.com>
2012-09-09 10:47:24 -0700

    Added configure.sh script that checks to make sure all system requirements are met.

---
+++ configure.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+fail=false
+
+printf "\nChecking requirements...\n\n"
+
+# Check for debugfs mount point
+echo -n "debugfs mounted..."
+
+if [ "$(mount | grep -c debugfs)" != 0 ]; then
+	echo "yes"
+else
+	fail=true
+	echo "no"
+fi
+
+# Check for python
+echo -n "python installed..."
+
+if [ "$(printf '%s\n' ${PATH//:/\/* } | grep -c python)" != 0 ]; then
+	echo "yes"
+else
+	fail=true
+	echo "no"
+fi
+
+# Check for pahole
+echo -n "pahole installed..."
+
+if [ "$(printf '%s\n' ${PATH//:/\/* } | grep -c pahole)" != 0 ]; then
+	echo "yes"
+else
+	fail=true
+	echo "no"
+fi
+
+# Report results
+echo ""
+
+if [ "$fail" == true ]; then
+	echo "Your system does not meet the requirements to run Loki."
+else
+	echo "System check complete. All requirements met."
+fi
+
+echo ""
